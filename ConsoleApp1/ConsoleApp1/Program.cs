a7feaf28000611f84b3c876560265d3585694ed6
PiotrW <126674277+Jasnyjan@users.noreply.github.com>
2023-03-25 17:22:00 +0100

    Dzień 4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int age = 30;
+string name = "Ewa";
+char sex = 'W';
+
+if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa lat 30");
+}
+else if (sex == 'W' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (age < 18 && sex == 'W')
+{
+    Console.WriteLine("Niepełnoletnia kobieta");
+}
